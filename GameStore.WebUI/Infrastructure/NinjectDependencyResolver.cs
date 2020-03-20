e0b2986ac652634a91163c06de2a06f3456f9367
home_pc <home_pc@ANASTASIA>
2020-03-20 21:02:35 +0300

    Добавьте файлы проекта.

---
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+
+namespace GameStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+    {
+        new Game { Name = "Дудлджамп", Price = 300 },
+        new Game { Name = "Три в ряд", Price=450 },
+        new Game { Name = "Том", Price=200 }
+    });
+            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+        }
+
+    }
+}
